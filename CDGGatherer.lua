fc855ddf9a97bd52254cdd2f6f729268eb4c55e4
simenon <albert@simenon.nl>
2014-04-10 11:55:42 +0200

    modified:   CDGGatherer.lua

--- CDGGatherer.lua
+++ CDGGatherer.lua
@@ -1,7 +1,14 @@
+local function CDGGathererInteractBusy()
+	if IsPlayerInteractingWithObject() and getInteractionType() == INTERACTION_HARVEST then
+		d(string.format("Player harvesting "))
+	end
+end
+
 local function CDGGathererLootReceived(eventCode, receivedBy, itemName, quantity, itemSound, lootType, self)
 	d(string.format("%d: %d %s looted by %s type %s [%s]",eventCode,quantity,itemName,receivedBy,lootType, tostring(self)    ))
 end
 
 function CDGGatherer_OnInitialized()
+	EVENT_MANAGER:RegisterForEvent("CDGGatherer",EVENT_INTERACT_BUSY, CDGGathererInteractBusy)
 	EVENT_MANAGER:RegisterForEvent("CDGGatherer",EVENT_LOOT_RECEIVED, CDGGathererLootReceived)
 end
