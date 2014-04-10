89e905e730b1749f82abc8b4a9450805d8f4d7aa
simenon <albert@simenon.nl>
2014-04-10 12:27:51 +0200

    dev commit

--- CDGGatherer.lua
+++ CDGGatherer.lua
@@ -1,7 +1,13 @@
+Player = {}
+
 function CDGGathererInteractBusy()
-	if IsPlayerInteractingWithObject() then
-		if (GetInteractionType() == INTERACTION_HARVEST) then
-			d(string.format("Player harvesting"))
+	if not Player.Harvesting then
+		if IsPlayerInteractingWithObject() then
+			if (GetInteractionType() == INTERACTION_HARVEST) then
+				Player.Harvesting = true
+				_, nodename = GetGameCameraInteractableInfo()
+				d(string.format("Player harvesting %s", nodename))
+			end
 		end
 	end
 end
@@ -12,9 +18,12 @@ function CDGGathererLootReceived(_, _, itemName, quantity, _, _, self)
 	end
 
 	d(string.format("looted %d %s",quantity,itemName ))
+	Player.Harvesting = false
 end
 
 function CDGGatherer_OnInitialized()
+	Player.Harvesting = false
+
 	EVENT_MANAGER:RegisterForEvent("CDGGatherer",EVENT_LOOT_RECEIVED, CDGGathererLootReceived)
 end
 
